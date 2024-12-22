96327396b899aa9571e3274478f1ea7b7435d7c1
Merna Mamdouh <mernamamdouh2@gmail.com>
2024-12-22 23:39:12 +0200

    edit in api call

--- src/apiCalls/articleApiCall.ts
+++ src/apiCalls/articleApiCall.ts
@@ -1,43 +1,40 @@
-import { Article } from "@prisma/client"
-import { DOMAIN } from "@/utils/constants"
-import { SingleArticle } from "@/utils/types"
+import { Article } from '@prisma/client';
+import { DOMAIN } from '@/utils/constants';
+import { SingleArticle } from '@/utils/types';
 
-// get articles based on pageNumber
-export async function getArticles(pageNumber: string | undefined): Promise<Article[]>{
-    const res = await fetch(`${DOMAIN}/api/articles?pageNumber=${pageNumber}`,
-        { cache: 'no-store' }
-    )
-    if(!res.ok){
-        throw new Error('Failed to fetch articles')
+// Get articles based on pageNumber
+export async function getArticles(pageNumber: string | undefined): Promise<Article[]> {
+    const res = await fetch(`${DOMAIN}/api/articles?pageNumber=${pageNumber}`,{ cache: 'no-store' });
+    if (!res.ok) {
+        throw new Error("Failed to fetch articles");
     }
-    return res.json()
+    return res.json();
 }
 
-// get articles count // not used this route (function)
-export async function getArticlesCount(): Promise<number>{
-    const res = await fetch(`${DOMAIN}/api/articles/count`,
-        { cache: 'no-store' })
-    if(!res.ok){
-        throw new Error('Failed to get articles count')
+// Get articles count
+export async function getArticlesCount(): Promise<number> {
+    const res = await fetch(`${DOMAIN}/api/articles/count`, { cache: 'no-store' });
+    if (!res.ok) {
+        throw new Error("Failed to get articles count");
     }
-    const {count} = await res.json() as {count:number};
+    const { count } = await res.json() as { count: number };
     return count;
 }
 
-// get articles based on searchText
-export async function getArticlesBasedOnSearch(searchText: string): Promise<Article[]>{
-    const res = await fetch(`${DOMAIN}/api/articles/search?searchText=${searchText}`)
-    if(!res.ok){
-        throw new Error('Failed to fetch articles')
+// Get articles based on searchText
+export async function getArticlesBasedOnSearch(searchText: string): Promise<Article[]> {
+    const res = await fetch(`${DOMAIN}/api/articles/search?searchText=${searchText}`);
+    if (!res.ok) {
+        throw new Error("Failed to fetch articles");
     }
-    return res.json()
+    return res.json();
 }
 
 // Get single article by id
 export async function getSingleArticle(articleId: string): Promise<SingleArticle> {
-    const response = await fetch(`${DOMAIN}/api/articles/${articleId}`, {cache: 'no-store'});
-    if (!response.ok) {
+    const res = await fetch(`${DOMAIN}/api/articles/${articleId}`, {cache: 'no-store'});
+    if (!res.ok) {
         throw new Error("Failed to fetch article");
     }
-    return response.json();
+    return res.json();
 }
